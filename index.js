675781df318cd1f6fb7cc4680d4eabaa5eacf762
Lucas <joselucasrs@gmail.com>
2020-12-01 03:19:00 -0300

    updated to get bot token from env

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ client.on('message', (message) => {
 });
 
 // Log our bot in using the token from https://discord.com/developers/applications
-client.login('ETv4kQqxCLNQLKxmn1T44jvhT1sdjscA');
+client.login(process.env.BOT_TOKEN);
