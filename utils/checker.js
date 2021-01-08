3086516cce93737f3698fa461b705b98dbba5977
Lucas <joselucasrs@gmail.com>
2021-01-08 18:25:29 -0300

    Error fixed now I think

--- utils/checker.js
+++ utils/checker.js
@@ -1,8 +1,7 @@
 const checkRole = (msg) => {
   if (
-    !msg.member.roles.cache.find(
-      (r) => r.id === '797024961738178590' || msg.channel.type === 'dm',
-    )
+    msg.channel.type === 'dm' ||
+    !msg.member.roles.cache.find((r) => r.id === '797024961738178590')
   )
     return 'noPerm';
 };
