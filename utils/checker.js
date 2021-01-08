423a9248365f93f637c0ad878e0b2616755df80e
Lucas <joselucasrs@gmail.com>
2021-01-08 18:13:43 -0300

    Check for dm

--- utils/checker.js
+++ utils/checker.js
@@ -1,5 +1,9 @@
 const checkRole = (msg) => {
-  if (!msg.member.roles.cache.find((r) => r.id === '797024961738178590'))
+  if (
+    !msg.member.roles.cache.find(
+      (r) => r.id === '797024961738178590' || msg.channel.type === 'dm',
+    )
+  )
     return 'noPerm';
 };
 
