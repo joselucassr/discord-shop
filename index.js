600043a9c6f048853ef0de16ee132b402274c0dd
Lucas <joselucasrs@gmail.com>
2021-01-08 05:43:15 -0300

    nhe

--- index.js
+++ index.js
@@ -103,11 +103,7 @@ client.on('message', async (msg) => {
       }
 
       await msg.channel.send(
-        `
-        Evento: "${checkReturn.eventName}" 
-        Membros: ${checkReturn.memberCount}
-        Recebendo resposta: ${checkReturn.acceptMsg}
-        `,
+        `Evento: "${checkReturn.eventName}" \nMembros: ${checkReturn.memberCount} \nRecebendo resposta: ${checkReturn.acceptMsg}`,
       );
       break;
     }
